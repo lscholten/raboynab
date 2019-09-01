9b418c4e1062ac61017537e1598a37af9e5c7469
Luuk Scholten <info@luukscholten.com>
2019-09-01 21:09:33 +0200

    Add support for converting RaboCard creditcard transactions

--- convert.py
+++ convert.py
@@ -2,28 +2,54 @@ from os.path import basename, splitext
 import pandas as pd
 import click
 
+
+def convert_credit_account(df):
+    num_entries = len(df)
+    return pd.DataFrame({
+        'Account': [f"{name}-{number}" for name, number in zip(df['Productnaam'], df['Creditcard Nummer'])],
+        'Date': [x.strftime('%Y-%m-%d') for x in pd.to_datetime(df['Datum'])],
+        'Payee': ['Creditcard'] * num_entries,
+        'Category': [''] * num_entries,
+        'Memo': df['Omschrijving'],
+        'Outflow': [-x if x < 0 else '' for x in df['Bedrag']],
+        'Inflow': [x if x >= 0 else '' for x in df['Bedrag']],
+    })
+
+
+def convert_regular_account(df):
+    return pd.DataFrame({
+        'Account': df['IBAN/BBAN'],
+        'Date': [x.strftime('%Y-%m-%d') for x in pd.to_datetime(df['Datum'])],
+        'Payee': [x if not pd.isnull(x) else '' for x in df['Naam tegenpartij']],
+        'Category': ['' for x in df['IBAN/BBAN']],
+        'Memo': df['Omschrijving-1'],
+        'Outflow': [x.replace('-', '').replace(',', '.') if '-' in x else '' for x in df['Bedrag']],
+        'Inflow': [x.replace('+', '').replace(',', '.') if '+' in x else '' for x in df['Bedrag']],
+    })
+
+
+
+def get_converter(rabo_df):
+    if 'Creditcard Nummer' in list(rabo_df.columns):
+        return convert_credit_account
+    return convert_regular_account
+
+
 @click.command()
 @click.argument('input_path', type=click.Path(exists=True))
 def convert(input_path):
     transactions_path = click.format_filename(input_path)
-    rabo = pd.read_csv(transactions_path, encoding = 'latin1')
+    rabo_df = pd.read_csv(transactions_path, encoding='latin1')
 
-    df_ynab = pd.DataFrame({
-        'Account': rabo['IBAN/BBAN'],
-        'Date': [x.strftime('%Y-%m-%d') for x in pd.to_datetime(rabo['Datum'])],
-        'Payee': [x if not pd.isnull(x) else '' for x in rabo['Naam tegenpartij']],
-        'Category': ['' for x in rabo['IBAN/BBAN']],
-        'Memo': rabo['Omschrijving-1'],
-        'Outflow': [x.replace('-', '').replace(',', '.') if '-' in x else '' for x in rabo['Bedrag']],
-        'Inflow': [x.replace('+', '').replace(',', '.') if '+' in x else '' for x in rabo['Bedrag']],
-    })
+    converter = get_converter(rabo_df)
+    df_ynab = converter(rabo_df)
 
     filename = splitext(basename(transactions_path))[0]
-
     for key, groupdf in df_ynab.groupby('Account'):
         fn = 'output/{}-{}.csv'.format(filename, key)
         print("Writing {} {} transactions".format(fn, len(groupdf)))
         groupdf.drop('Account', 1).to_csv(fn, index=0)
+
 
 if __name__ == '__main__':
     convert()
